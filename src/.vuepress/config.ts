66831673c869e8a410f58ad7c7610643fe47cf4a
mayunterry <1138344078@qq.com>
2025-08-28 20:59:02 +0800

    add blog base

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,20 +1,20 @@
-import { defineUserConfig } from "vuepress";
+import { defineUserConfig } from 'vuepress';
 
-import theme from "./theme.js";
+import theme from './theme.js';
 
 export default defineUserConfig({
-  base: "/",
+  base: '/blog/',
 
   locales: {
-    "/": {
-      lang: "en-US",
-      title: "Blog Demo",
-      description: "A blog demo for vuepress-theme-hope",
+    '/': {
+      lang: 'en-US',
+      title: 'Blog Demo',
+      description: 'A blog demo for vuepress-theme-hope',
     },
-    "/zh/": {
-      lang: "zh-CN",
-      title: "博客演示",
-      description: "vuepress-theme-hope 的博客演示",
+    '/zh/': {
+      lang: 'zh-CN',
+      title: '博客演示',
+      description: 'vuepress-theme-hope 的博客演示',
     },
   },
 
