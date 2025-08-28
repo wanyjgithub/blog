b684211bdb72b9f7dcd77977c301917bbb13f1a0
mayunterry <1138344078@qq.com>
2025-08-29 00:35:09 +0800

    set baseURL

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,13 +1,13 @@
-import { defineUserConfig } from "vuepress";
+import { defineUserConfig } from 'vuepress';
 
-import theme from "./theme.js";
+import theme from './theme.js';
 
 export default defineUserConfig({
-  base: "/",
+  base: '/blog/',
 
-  lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+  lang: 'zh-CN',
+  title: '博客演示',
+  description: 'vuepress-theme-hope 的博客演示',
 
   theme,
 
